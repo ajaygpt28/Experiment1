68f556fcbd681dd8461d1a9de0572baaeb79656a
Ajay Gupta <ajaygpt28@gmail.com>
2018-10-28 11:44:47 +0530

    first

---
+++ experiment/src/exp2/class2.java
@@ -0,0 +1,10 @@
+package exp2;
+
+public class class2 {
+	
+	public static void main(String[] args) {
+		int[] q[]={{11,22},{33,44,55},{66,77,88,99}};
+		int [][] j=q;
+		System.out.println(j[2][2]);
+	}
+}
